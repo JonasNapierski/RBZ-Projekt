2ea317bcffeeb29c8231efafe915f6c12bfcfb66
mjm <max-julian.machleid@ppimedia.de>
2025-09-09 09:06:35 +0200

    xml collector

--- Collectors/xmlCollector.cs
+++ Collectors/xmlCollector.cs
@@ -1,26 +1,45 @@
+using System.Collections.Generic;
 using System.Xml;
 
 public class xmlCollector
 {
-    public override importData()
+    private readonly SQLiteContext _context;
+
+    public XmlCollector(SQLiteContext context)
+    {
+        _context = context;
+    }
+
+    public void importData()
     {
         XmlDocument doc = new XmlDocument();
         doc.Load("..\\Data\\festivals.xml");
 
-        var festivals
+        List <Festival> festivals = new List<Festival>();
+
         foreach(XmlNode festivalNode in doc.DocumentElement.ChildNodes){
            string name = festivalNode.Attribute["name"].Value;
-           int year = festivalNode.Attribute["year"].Value;
+           int year = int.Parse(festivalNode.Attribute["year"].Value;
            string location = festivalNode.Attribute["location"].Value;
-           new Festival()
+            List<Movie> movies = new List<Movie>();
 
+           Festival festival = new Festival(year, name, location, movies);
+           
            foreach(XmlNode movieNode in festivalNode.ChildNodes)
            {
-                string id = movieNode.Attribute["id"].Value
-                string category = movieNode.Attribute["category"].Value
-                string status = movieNode.Attribute["status"].Value
-                string name
+                string id = movieNode.Attribute["id"].Value;
+                string category = movieNode.Attribute["category"].Value;
+                string status = movieNode.Attribute["status"].Value;
+                string name = movieNode.InnerText;
+                
+                //todo: search for movie in DbContext bevore creating new Movie.
+                //      are movies even saved with only .SaveChanges()?
+                Movie movie = new Movie(id, category, status, name);
+
+                festival.movies.Add(movie);
            }
+            _context.Festival.Add(festival);
         }
+        _context.SaveChanges();
     }
 }
